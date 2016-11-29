ece93849422b301af83285c67fa103b588f60db0
diogorusso <hello@diogorusso.com>
2016-11-28 21:58:00 -0800

    Update 2016-11-29T05:53:55.503Z

--- gulp/tasks/development/yaml-bundle.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var yamlMerge = require('gulp-yaml-merge');
-
-gulp.task('yaml', function() {
-  return gulp.src('./_dev/*.yml')
-    .pipe(yamlMerge('bundle.yml'))
-    .pipe(gulp.dest('./dist/'));
-});
