7d06e27da718bf9e801a79014db295713ce4b7f7
diogorusso <hello@diogorusso.com>
2016-12-05 22:17:10 -0800

    yaml bundle task gulp

--- gulp/tasks/development/yaml.js
+++ gulp/tasks/development/yaml.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var yamlMerge = require('gulp-yaml-merge');
 
 gulp.task('yaml', function() {
-  return gulp.src('./_dev/*.yml')
-    .pipe(yamlMerge('bundle.yml'))
-    .pipe(gulp.dest('./dist/'));
+  return gulp.src('./_config/*.yml')
+    .pipe(yamlMerge('#bundle.yml'))
+    .pipe(gulp.dest('./_dev/'));
 });
