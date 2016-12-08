4a4fbcc35debbef18a9a5c722411693324eb3377
diogorusso <hello@diogorusso.com>
2016-12-07 18:47:04 -0800

    gulp image task

--- gulp/tasks/design/images.js
+++ gulp/tasks/design/images.js
@@ -4,55 +4,55 @@ var imageOp = require('gulp-image-optimization');
 var imageResize = require('gulp-image-resize');
 var rename = require("gulp-rename");
 var clean = require('gulp-clean');
+var imgSrc = './_dev/_assets/_img/';
+var imgSrcResp = './_dev/_assets/_img/responsive/' ;
+var imgDest = './_dev/lib/img/';
+var imgBreakpoints = {
+  sizes: [480,791,1042,1482,1920],
+};
+
 
 gulp.task('site-imgs', function() {
   runSequence(
-              'imgs-clean',
+              'site-imgs-clean',
               'sketch',
-              'imgs-base-copy',
-              'imgs-components-copy',
-              'imgs-base-responsive',
-              'imgs-components-responsive'
+              'imgs-copy',
+              'imgs-resp'
         );
 });
 
 gulp.task('site-imgs-clean', function () {
   return gulp.src([
-    './_dev/_assets/sketch',
-    './_dev/lib/imgs/sketch/'], 
+    imgSrc,
+    imgDest], 
     {read: true})
     .pipe(clean());
 });
 
-gulp.task('imgs-base-copy', function() {
+gulp.task('imgs-copy', function() {
   return gulp.src([
-  		'./_dev/_assets/sketch/base/*.*',
-      './_dev/_assets/sketch/base/responsive/*.*',
+  		imgSrc+'*.*',
+      imgSrcResp+'*.*',
   	])
-    .pipe(gulp.dest('./_dev/lib/imgs/sketch/base'));
+    
+    .pipe(gulp.dest(imgDest));
 });
 
-gulp.task('imgs-components-copy', function() {
-  return gulp.src([
-      './_dev/_assets/sketch/components/*.*',
-      './_dev/_assets/sketch/components/responsive/*.*',
-  	])
-    .pipe(gulp.dest('./_dev/lib/imgs/sketch/components'));
-});
 
-gulp.task("imgs-base-responsive", function () {
+
+gulp.task("imgs-resp", function () {
 
  var imgSrcPaths ={
-    src: ['_dev/_assets/sketch/base/responsive/*.*'],
-    dest: '_dev/lib/imgs/sketch/base'
+    src: imgSrcResp+'*.*',
+    dest: imgDest
   };
 
   function GenImages(value){
     gulp.src(imgSrcPaths.src)
     .pipe(imageResize({ width : value }))
     .pipe(rename(function (path) { 
-      path.basename += '_'+value+'px';
-      //path.dirname += "/"+ value;
+      //path.basename += '_'+value+'px';
+      path.dirname += "/"+ value;
     }))
     .pipe(imageOp({
           optimizationLevel: 7,
@@ -62,62 +62,13 @@ gulp.task("imgs-base-responsive", function () {
     .pipe(gulp.dest(imgSrcPaths.dest));
   }
 
-
-   var imgBreakpoints = {
-    sizes: [350,795,1118,1511,1826,2147,2450],
-  };
-
-  
   for ( var i = 0; i < imgBreakpoints.sizes.length; i++) {
     var objs = imgBreakpoints.sizes[i];
     console.log(imgSrcPaths.src+ '------' + objs);
-
-    // 5min LOOP
     
     GenImages(objs)
     }
 });
-
-gulp.task("imgs-components-responsive", function () {
-
-
- var imgSrcPaths ={
-    src: ['_dev/_assets/sketch/components/responsive/*.*'],
-    dest: '_dev/lib/imgs/sketch/components'
-  };
-
-  function GenImages(value){
-    gulp.src(imgSrcPaths.src)
-    .pipe(imageResize({ width : value }))
-    .pipe(rename(function (path) { 
-      path.basename += '_'+value+'px';
-      //path.dirname += "/"+ value;
-    }))
-    .pipe(imageOp({
-          optimizationLevel: 7,
-          progressive: true,
-          interlaced: true
-    }))
-    .pipe(gulp.dest(imgSrcPaths.dest));
-  }
-
-
-   var imgBreakpoints = {
-    sizes: [350,795,1118,1511,1826,2147,2450],
-  };
-
-  
-  for ( var i = 0; i < imgBreakpoints.sizes.length; i++) {
-    var objs = imgBreakpoints.sizes[i];
-    console.log(imgSrcPaths.src+ '------' + objs);
-
-    // 5min LOOP
-    
-    GenImages(objs)
-    }
-});
-
-
 
 
 
--- gulp/tasks/design/sketch-app.js
+++ gulp/tasks/design/sketch-app.js
@@ -1,10 +1,11 @@
 var gulp   = require('gulp');
 var sketch = require('gulp-sketch');
+var imgSrc = './_dev/_assets/_img/';
 
 gulp.task('sketch', function(){
   return gulp.src('./_design/Layout_Foundation_5.sketch')
     .pipe(sketch({
       export: 'slices',
     }))
-    .pipe(gulp.dest('./_dev/_assets/sketch'));
+    .pipe(gulp.dest(imgSrc));
 });
